73dcdde1413d257455523270c46854b39c9679bf
MilhamAt <M477ashiddiq@gmail.com>
2020-12-17 22:38:32 +0700

    Initial Commit

---
+++ LoadURLimage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoadURLimage
+//
+//  Created by Muhammad Ilham Ashiddiq Tresnawan on 17/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
